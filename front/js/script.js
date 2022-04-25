77b06903b98a9172ae9465c4e9c3a1e0b878be0d
Marion Devroede <marion.devroede@hotmail.fr>
2022-04-25 18:57:59 +0200

    Modif du script

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,16 @@
 async function getProducts() { 
-    fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function(value) {
-      console.log(value);
-    })
-    .catch(function(err) {
-      // Une erreur est survenue
-    });
+  fetch("http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(Product) {
+    console.log(Product);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
 
 }
 
@@ -26,19 +26,21 @@ a.appendChild(article);
 
 const img = document.createElement('img');
 article.appendChild(img);
-
+img.src = getProducts().imageUrl;
+img.alt = getProducts().altTxt;
 
 
 const h3 = document.createElement('h3');
 article.appendChild(h3);
 h3.classList.add('productName');
+h3.innerText = getProducts().name; 
 
 
 
 const p = document.createElement('p');
 article.appendChild(p);
 p.classList.add('productDescription');
-
+p.innerText = getProducts().description;
 
 
 
