5247bfcfb7fb9e301811dda5e8f55f75dbd4cb0d
Marion Devroede <marion.devroede@hotmail.fr>
2022-04-26 17:46:06 +0200

    Ajout de la fonction foreach

--- front/js/script.js
+++ front/js/script.js
@@ -1,46 +1,58 @@
+//Récuppération de l'API
+
 async function getProducts() { 
   fetch('http://localhost:3000/api/products')
   .then(function(res) {
-    if (response.ok) {
-      return response.json();
+      if (response.ok) {
+          return response.json();
     }
   })
+
   .then(function(Produits) {
-    console.log('Produits');
-  })
+      Produits.forEach(products =>  {
+          console.log('Produits');
+          
+          
+
+// Création du lien
+              let section = document.getElementById('items');
+              let a = document.createElement('a');
+              section.appendChild(a);
+              a.href = './product.html?id=42';
+
+//Création de l'article
+              let article = document.createElement('article');
+              a.appendChild(article);
+
+//Création de l'image
+              let img = document.createElement('img');
+              article.appendChild(img);
+              img.src = products.imageUrl;
+              img.alt = products.altTxt;
+
+//Création du titre
+              let h3 = document.createElement('h3');
+              article.appendChild(h3);
+              h3.classList.add('productName');
+              h3.innerText = products.name; 
+
+//Création de la description
+              let p = document.createElement('p');
+              article.appendChild(p);
+              p.classList.add('productDescription');
+              p.innerText = products.description;
+          
+      });
+    })
+
+//Affichage du message d'erreur
   .catch(function(erreur) {
-    console.log('Une erreur est survenue' + erreur.message);
+      console.log('Une erreur est survenue' + erreur.message);
   });
+  
 
 }
 
-const section = document.getElementById('items');
-const a = document.createElement('a');
-section.appendChild(a);
-a.href = './product.html?id=42';
-
-
-const article = document.createElement('article');
-a.appendChild(article);
-
-
-const img = document.createElement('img');
-article.appendChild(img);
-img.src = getProducts().imageUrl;
-img.alt = getProducts().altTxt;
-
-
-const h3 = document.createElement('h3');
-article.appendChild(h3);
-h3.classList.add('productName');
-h3.innerText = getProducts().name; 
-
-
-
-const p = document.createElement('p');
-article.appendChild(p);
-p.classList.add('productDescription');
-p.innerText = getProducts().description;
 
 
 
