8bfac965c990cfe29d068b135540d8e7c953598f
Marion Devroede <marion.devroede@hotmail.fr>
2022-04-26 17:57:07 +0200

    modification du lien

--- front/js/script.js
+++ front/js/script.js
@@ -10,38 +10,39 @@ async function getProducts() {
 
   .then(function(Produits) {
       Produits.forEach(products =>  {
-          console.log('Produits');
+         
           
           
 
 // Création du lien
-              let section = document.getElementById('items');
-              let a = document.createElement('a');
-              section.appendChild(a);
-              a.href = './product.html?id=42';
+          let section = document.getElementById('items');
+          let a = document.createElement('a');
+          section.appendChild(a);
+          a.href = products._id;
 
 //Création de l'article
-              let article = document.createElement('article');
-              a.appendChild(article);
+          let article = document.createElement('article');
+          a.appendChild(article);
 
 //Création de l'image
-              let img = document.createElement('img');
-              article.appendChild(img);
-              img.src = products.imageUrl;
-              img.alt = products.altTxt;
+          let img = document.createElement('img');
+          article.appendChild(img);
+          img.src = products.imageUrl;
+          img.alt = products.altTxt;
 
 //Création du titre
-              let h3 = document.createElement('h3');
-              article.appendChild(h3);
-              h3.classList.add('productName');
-              h3.innerText = products.name; 
+          let h3 = document.createElement('h3');
+          article.appendChild(h3);
+          h3.classList.add('productName');
+          h3.innerText = products.name; 
 
 //Création de la description
-              let p = document.createElement('p');
-              article.appendChild(p);
-              p.classList.add('productDescription');
-              p.innerText = products.description;
+          let p = document.createElement('p');
+          article.appendChild(p);
+          p.classList.add('productDescription');
+          p.innerText = products.description;
           
+          console.log('Produits');
       });
     })
 
