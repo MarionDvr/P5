0445b7765cb3a0782cae5515d631393d84ae280d
Marion Devroede <marion.devroede@hotmail.fr>
2022-05-05 07:38:09 +0200

    ajout du html et de l'api

---
+++ front/js/cart.js
@@ -0,0 +1,92 @@
+//Récupération de l'API
+fetch("http://localhost:3000/api/products")
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+
+//Répartition des valeurs du tableau sur les élément HTML concerant le canapé
+    .then(function(Canape) {
+        Canape.forEach(products =>  {
+            
+//Récupération du local storage
+            productID = localStorage.getItem('id');
+            productColor = localStorage.getItem('Couleur');
+            ChoixQuantité = localStorage.getItem('Nombre');
+
+
+        //Création de l'article
+        let sectionCartItems = document.getElementById('cart__items');
+        let article = document.createElement('article');
+        article.classList.add('cart__item');
+        article.dataset.id = 'productID';
+        article.dataset.color = 'productColor'
+        sectionCartItems.appendChild(article);
+
+        //Création de la div contenant l'image
+        let divItemImage = document.createElement('div');
+        divItemImage.classList.add('cart__item__img');
+        sectionCartItems.appendChild(divItemImage);
+        //Image
+        let ImageItem = document.createElement('img');
+        ImageItem.src = products.imageUrl;
+        ImageItem.alt = products.altTxt;
+        divItemImage.appendChild(ImageItem);
+
+        //Création de la div contenant les détails du produit
+        //div parent
+        let divItemContent = document.createElement('div');
+        divItemContent.classList.add('cart__item__content');
+        article.appendChild(divItemContent);
+        //div enfant
+        let divItemDescription = document.createElement('div');
+        divItemDescription.classList.add('cart__item__content__description');
+        divItemContent.appendChild(divItemDescription);
+        //Titre
+        let h2 = document.createElement('h2');
+        divItemDescription.appendChild(h2);
+        h2.innerHTML = products.name;
+        //Couleur
+        let pColor = document.createElement('p');
+        divItemDescription.appendChild(pColor);
+        pColor.innerHTML = productColor;
+        //Prix
+        let pPrice = document.createElement('p');
+        divItemDescription.appendChild(pPrice);
+        pPrice.innerHTML = products.price;
+
+        //Création de la div contenant la quantité et la suppression
+        //div parent
+        let divItemSettings = document.createElement('div');
+        divItemSettings.classList.add('cart__item__content__settings');
+        article.appendChild(divItemSettings);
+        //Création de la div contenant la quantité - div enfant 1
+        let divItemsSettingsQuantity = document.createElement('div');
+        divItemsSettingsQuantity.classList.add('cart__item__content__settings__quantity');
+        divItemSettings.appendChild(divItemsSettingsQuantity);
+        // paragraphe
+        let pQuantité = document.createElement('p');
+        divItemsSettingsQuantity.appendChild(pQuantité);
+        //input
+        let inputItemQuantity = document.createElement('input');
+        inputItemQuantity.classList.add('itemQuantity');
+        divItemsSettingsQuantity.appendChild(inputItemQuantity);
+        inputItemQuantity.innerHTML = ChoixQuantité;
+
+        //Création de la suppression
+        //div enfant 2
+        let divItemDelete = document.createElement('div');
+        divItemDelete.classList.add('cart__item__content__settings__delete');
+        divItemSetting.appendChild(divItemDelete);
+        //Paragraphe
+        let pDelete = document.createElement('p');
+        pDelete.classList.add('deleteItem');
+        pDelete.innerHTML = "Supprimer";
+        divItemDelete.appendChild(pDelete);
+
+        }   
+
+    )
+
+})
