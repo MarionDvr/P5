533fb29a9db7cc632fb633e7677f04d7212a13d8
Marion Devroede <marion.devroede@hotmail.fr>
2022-07-18 19:03:57 +0200

    Récupération de l'id de confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,7 @@
 //Récupération de l'emplacement de l'id dans le html
 const orderId = document.getElementById('orderId');
-//Récupération de l'id de la commande
+//Récupération de l'id de la commande dans l'url
 const id = window.location.search.split("?").join("");
-console.log(id)
+
 
 orderId.innerHTML = id;
